5d5050de0f13686720cd7b99675f2074b7988d26
Roberto Guerra <prosaka@hotmail.com>
2019-05-07 23:58:03 -0300

    New: Create component Credit Card

--- src/components/generateCpf.js
+++ src/components/generateCpf.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import "../css/style.css";
 import helper from '../Helpers/createCpf'
-import create from '../Helpers/createCreditCard'
+import createCard from '../Helpers/createCard'
+import '../css/style.css';
+import amex from'../images/amex.png'
 
 export default class GenerateCpf extends Component {
     constructor(props) {
@@ -11,10 +12,22 @@ export default class GenerateCpf extends Component {
         this.createCNPJ = this.createCNPJ.bind(this);
         this.copy = this.copy.bind(this);
         this.useMask = this.useMask.bind(this);
+        this.newCard = this.newCard.bind(this)
+        this.setHash = this.setHash.bind(this)
 
         this.state = {
-            document: "",
-            mask: localStorage.getItem('mask') === 'true' ? true : false
+            hash: 'cpf',
+
+            document: '',
+            mask: localStorage.getItem('mask') === 'true' ? true : false,
+
+            type: '',
+            number: '',
+            expDate: '',
+            cvv: '',
+            flags: ['visa', 'master', 'amex', 'discover' ],
+            image: amex
+
         };
     }
 
@@ -22,16 +35,40 @@ export default class GenerateCpf extends Component {
         e.preventDefault();
 
         this.setState({ document: helper.gerarCPF(this.state.mask) });
-
-        this.copy(e)
     }
 
     createCNPJ(e) {
         e.preventDefault();
-        console.log(create.visa())
-        this.setState({ document: helper.gerarCNPJ(this.state.mask) });
 
-        this.copy(e)
+        this.setState({ document: helper.gerarCNPJ(this.state.mask) });
+    }
+
+    newCard(e) {
+
+        this.setState({
+            type: e.target.value,
+            expDate: createCard.exp(),
+            cvv: createCard.cvv(),
+        })
+
+        console.log(e.target.value);
+
+        // eslint-disable-next-line default-case
+        switch (e.target.value) {
+            case 'visa':
+                this.setState({ number: createCard.visa() })
+                break;
+            case 'master':
+                this.setState({ number: createCard.masterCard() })
+                break;
+            case 'discover':
+                this.setState({ number: createCard.discover() })
+                break;
+            case 'amex':
+                this.setState({ number: createCard.amex() })
+                break;
+
+        }
     }
 
     useMask(e) {
@@ -45,31 +82,84 @@ export default class GenerateCpf extends Component {
         document.execCommand('copy');
     }
 
-    componentDidUpdate() {
-        this.refs.input.value = this.state.document
-        this.copy();
+    setHash(e) {
+        this.setState({ hash: e.target.value })
     }
 
+  
+
     render() {
+
+        const button = this.state.flags.map((flags, index) => {
+
+           return  <button key={index} value={flags} onClick={this.newCard} style={{ 
+            backgroundImage: `url("../images/${flags}.png")`,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat' }} >{flags}</button> 
+                
+        });
+
         return (
-            <div className="App">
+            <div className="main">
 
-                <div className="btn-selector">
-                    <button className="btn-cpf" id="btn-cpf" onClick={this.createCPF}>CPF</button>
-                    <button className="btn-cnpj" id="btn-cnpj" onClick={this.createCNPJ}>CNPJ</button>
+                <div className="menu">
+                    <button value='cpf' onClick={this.setHash}>CPF</button>
+                    <button value='card' onClick={this.setHash}>Cartão</button>
                 </div>
+                {
+                    this.state.hash === 'cpf' ?
 
-                <div className="mask">
-                    <input type="checkbox" ref="mask" defaultChecked={this.state.mask} onClick={this.useMask} />
-                    <span>Usar máscara</span>
-                </div>
+                        <div className="cpf">
 
-                <div className="input-result">
-                    <input type="text" ref="input" defaultValue={this.state.document} onClick={this.copy} />
-                    <span style={{ visibility: this.state.document ? 'visible' : 'hidden' }}>Copiado!</span>
-                </div>
+                            <div className="input-result">
+                                <input type="text" ref="input" defaultValue={this.state.document} onClick={this.copy} />
+
+                            </div>
+
+                            {/* <div className="copied">
+                                <span style={{ display: this.state.document ? 'block' : 'none' }}>Copiado</span>
+                            </div> */}
+                            <div className="mask">
+                                <input type="checkbox" ref="mask" defaultChecked={this.state.mask} onClick={this.useMask} />
+                                <span>Usar máscara</span>
+                            </div>
+
+                            <div className="btn-selector">
+                                <button className="btn-cpf" id="btn-cpf" onClick={this.createCPF}>CPF</button>
+                                <button className="btn-cnpj" id="btn-cnpj" onClick={this.createCNPJ}>CNPJ</button>
+                            </div>
+
+
+
+                        </div>
+                        :
+
+                        <div className="card">
+                            <div>
+                                {button}
+                            </div>
+
+                            <div>
+                                <div className='input-result' >
+                                    <label>Cartão</label>
+                                    <input defaultValue={this.state.number} />
+                                </div>
+                                <div className='input-result' >
+                                    <label>Exp</label>
+                                    <input defaultValue={this.state.expDate} />
+                                </div>
+                                <div className='input-result' >
+                                    <label>Cvv</label>
+                                    <input defaultValue={this.state.cvv} />
+                                </div>
+                            </div>
+                        </div>
+                }
+
 
             </div>
+
         );
     }
 }
