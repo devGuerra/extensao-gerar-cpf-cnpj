e7773809ee85e1aba4daa215187fe75eb3ecb497
Roberto Guerra <prosaka@hotmail.com>
2019-05-07 23:58:03 -0300

    New: Create component Credit Card

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GenerateCpf from './components/generateCpf';
-import CreateCreditCard from '../src/components/generateCard'
 
 ReactDOM.render(
-    // <GenerateCpf />, 
-    <CreateCreditCard />,
+    <GenerateCpf />, 
+    // <CreateCreditCard />,
     document.getElementById('root')
 );
