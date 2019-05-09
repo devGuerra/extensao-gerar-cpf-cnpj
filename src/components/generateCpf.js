270e1da4cdbae98cb16758858c9b587ac7c2c1ae
Roberto Guerra <prosaka@hotmail.com>
2019-05-08 23:37:47 -0300

    Add: fix functions and set creditCard

--- src/components/generateCpf.js
+++ src/components/generateCpf.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import helper from '../Helpers/createCpf'
 import createCard from '../Helpers/createCard'
 import '../css/style.css';
-import amex from'../images/amex.png'
 
 export default class GenerateCpf extends Component {
     constructor(props) {
@@ -14,22 +13,25 @@ export default class GenerateCpf extends Component {
         this.useMask = this.useMask.bind(this);
         this.newCard = this.newCard.bind(this)
         this.setHash = this.setHash.bind(this)
+        this.clearState = this.clearState.bind(this)
+        this.cardSelected = this.cardSelected.bind(this)
 
         this.state = {
-            hash: 'cpf',
+            hash: localStorage.getItem('hash') ||  '',
 
             document: '',
             mask: localStorage.getItem('mask') === 'true' ? true : false,
 
-            type: '',
-            number: '',
-            expDate: '',
-            cvv: '',
-            flags: ['visa', 'master', 'amex', 'discover' ],
-            image: amex
+            type: localStorage.getItem('type') ||  '',
+            number:localStorage.getItem('number') || '',
+            expDate: localStorage.getItem('exp') || '',
+            cvv: localStorage.getItem('cvv') || '',
+
+            flags: ['visa', 'master', 'amex', 'discover' ]
 
         };
     }
+
 
     createCPF(e) {
         e.preventDefault();
@@ -43,7 +45,26 @@ export default class GenerateCpf extends Component {
         this.setState({ document: helper.gerarCNPJ(this.state.mask) });
     }
 
+    cardSelected(e){
+        const { className } = e.target
+
+        if (className !== this.state.type) {
+            console.log(e.target)
+        }
+        
+        
+    }
+
     newCard(e) {
+        const { target, className } = e
+
+        if(className !== this.state.type){
+            target.classList.add('selected')
+        } else {
+            target.classList.remove('selected')
+        }
+
+        
 
         this.setState({
             type: e.target.value,
@@ -51,9 +72,6 @@ export default class GenerateCpf extends Component {
             cvv: createCard.cvv(),
         })
 
-        console.log(e.target.value);
-
-        // eslint-disable-next-line default-case
         switch (e.target.value) {
             case 'visa':
                 this.setState({ number: createCard.visa() })
@@ -67,8 +85,9 @@ export default class GenerateCpf extends Component {
             case 'amex':
                 this.setState({ number: createCard.amex() })
                 break;
-
+            default:
         }
+
     }
 
     useMask(e) {
@@ -78,34 +97,65 @@ export default class GenerateCpf extends Component {
     }
 
     copy(e) {
-        this.refs.input.select();
+        e.target.select();
         document.execCommand('copy');
     }
 
-    setHash(e) {
-        this.setState({ hash: e.target.value })
+    clearState(e){
+        this.setState({
+            document: '',
+            type: '',
+            number:'',
+            expDate: '',
+            cvv: '',
+        })
     }
 
-  
+    setHash(e) {
+
+        this.setState({ hash: e.target.value })
+
+        this.clearState()
+    }
+
+    componentDidUpdate(){
+
+        if(this.state.hash === 'cpf'){
+            this.refs.input.value = this.state.document
+            this.refs.input.select()
+            document.execCommand('copy')
+        } else {
+            this.refs.cardNumber.value = this.state.number
+            this.refs.cardDate.value = this.state.expDate
+            this.refs.cardCvv.value = this.state.cvv
+        }
+
+        localStorage.setItem('hash', this.state.hash)
+        localStorage.setItem('type', this.state.type)
+        localStorage.setItem('number', this.state.number)
+        localStorage.setItem('exp', this.state.expDate)
+        localStorage.setItem('cvv', this.state.cvv)
+        
+    }
+
+    componentDidMount(){
+        console.log(this.state);
+    }
 
     render() {
 
         const button = this.state.flags.map((flags, index) => {
 
-           return  <button key={index} value={flags} onClick={this.newCard} style={{ 
-            backgroundImage: `url("../images/${flags}.png")`,
-            backgroundPosition: 'center',
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat' }} >{flags}</button> 
-                
+           return  <button key={index} className={flags === this.state.type ? `${flags} selected` : flags} value={flags} onClick={this.newCard} />
+            
         });
 
         return (
             <div className="main">
 
                 <div className="menu">
-                    <button value='cpf' onClick={this.setHash}>CPF</button>
-                    <button value='card' onClick={this.setHash}>Cartão</button>
+                    <button value='cpf' onClick={this.setHash}>Dados Pessoais</button>
+                    <button value='card'  onClick={this.setHash}>Cartão de credito</button>
                 </div>
                 {
                     this.state.hash === 'cpf' ?
@@ -117,9 +167,9 @@ export default class GenerateCpf extends Component {
 
                             </div>
 
-                            {/* <div className="copied">
+                            <div className="copied">
                                 <span style={{ display: this.state.document ? 'block' : 'none' }}>Copiado</span>
-                            </div> */}
+                            </div>
                             <div className="mask">
                                 <input type="checkbox" ref="mask" defaultChecked={this.state.mask} onClick={this.useMask} />
                                 <span>Usar máscara</span>
@@ -143,15 +193,15 @@ export default class GenerateCpf extends Component {
                             <div>
                                 <div className='input-result' >
                                     <label>Cartão</label>
-                                    <input defaultValue={this.state.number} />
+                                    <input ref='cardNumber' defaultValue={this.state.number} onClick={this.copy}  />
                                 </div>
                                 <div className='input-result' >
                                     <label>Exp</label>
-                                    <input defaultValue={this.state.expDate} />
+                                    <input ref='cardDate'  defaultValue={this.state.expDate} onClick={this.copy} />
                                 </div>
                                 <div className='input-result' >
                                     <label>Cvv</label>
-                                    <input defaultValue={this.state.cvv} />
+                                    <input ref='cardCvv'  defaultValue={this.state.cvv} onClick={this.copy} />
                                 </div>
                             </div>
                         </div>
